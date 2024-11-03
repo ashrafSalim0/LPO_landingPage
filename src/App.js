959edb71864318fb62108451c9c0dbb1bd070565
ashrafSalim0 <oxroxi@proton.me>
2024-11-03 08:34:13 +0200

    ashraf commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a test</h1>
+      <i style={{fontSize: 100, color: 'purple'}} class="ti ti-planet"></i>
+    </div>
+  );
+}
+
+export default App;
