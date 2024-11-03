5e2b2577c79a5bec2181327539cc7cc7ddf6c0d8
ashrafSalim0 <oxroxi@proton.me>
2024-11-03 10:30:23 +0200

    ashraf commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function menubutton(){
+  console.log('clicked')
+  document.querySelector('.short-line').classList.toggle('short-line-active')
+  document.querySelector('.long-line').classList.toggle('long-line-active')
+  document.querySelector('.last-line').classList.toggle('last-line-active')
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>This is a test</h1>
-      <i style={{fontSize: 100, color: 'purple'}} class="ti ti-planet"></i>
+    <>
+         
+    <div className='main-screen'>
+    <div className='nav-bar'>
+      <button className='menu-button-container' onClick={() => menubutton()}>
+        <div className='menu-button-line short-line'></div>
+        <div className='menu-button-line long-line'></div>
+        <div className='menu-button-line short-line last-line'></div>
+      </button>
+     </div>
+      <div className='scroll-mouse'>
+        <div></div>
+      </div>
+      <div style={{display: 'flex', gap: '1.5rem', position: 'absolute', bottom: '3rem', left: '3rem'}}>
+          <button className='button-navigation'>Projects</button>
+          <button className='button-navigation'>About</button>
+        </div>
+        <div className='profile-pic-container'>
+        <img src="./assets/gif/techny-rocket.gif" alt="" className='rocket-gif' />
+        <img src="./assets/gif/techny-rocket.gif" alt="" className='rocket-gif outer-gif' />
+
+        </div>  
+
+        <p className='main-width-text'> my expertise lies in<br></br> turning <span className='header__hero--heading-gradient'>creative ideas</span> <br></br> into actual products.</p>
     </div>
+    <div className='second-screen'></div>
+   
+
+    </>
   );
 }
 
